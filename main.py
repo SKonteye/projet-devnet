02cb6878288c7b9e4ec1539a0672cccf1664054a
NetAcad DEVASC <changeme@example.com>
2024-04-04 16:47:53 +0000

    main

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+
+def get_ip_info():
+    try:
+        # Appel à l'API ipapi.co pour obtenir les informations d'adresse IP
+        response = requests.get('https://ipapi.co/json/')
+        data = response.json()
+        # print(data)
+        # Extraction des informations pertinentes
+        ip_address = data['ip']
+        version = data['version']
+        currency = data['currency']
+        country = data['country']
+        city = data['city']
+        latitude = data['latitude']
+        longitude = data['longitude']
+        isp = data['org']
+        asn = data['asn']
+        
+        print(data)
+    except Exception as e:
+        print(f"Erreur lors de la récupération des informations d'adresse IP: {e}")
+
+if __name__ == "__main__":
+    get_ip_info()
